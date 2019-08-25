778080a4bdab916c79ad94b4a02a8c02cc10e452
PC <PC@DESKTOP-FSOCBLA>
2019-08-25 18:26:20 +0300

    Added Shell Sort

---
+++ SortingAlgorithms/Algorithms/ShellSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAlgorithms.Algorithms
+{
+    public class ShellSort<T> : BaseAlgorithm<T> where T : IComparable
+    {
+        public override void Sort()
+        {
+            var step = Items.Count / 2;
+
+            while (step > 0)
+            {
+                for (int i = step; i < Items.Count; i++)
+                {
+                    int j = i;
+                    while (j >= step && Items[j - step].CompareTo(Items[j]) > 0)
+                    {
+                        Swap(j- step, j);
+                        j -= step;
+                    }
+                }
+                step /= 2;
+            }
+        }
+    }
+}
--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -7,65 +7,86 @@ namespace SortingAlgorithms
         static void Main(string[] args)
         {
             #region BubbleSort
-            Console.WriteLine("Bubble Sort:");
-            var bubble = new Algorithms.BubbleSort<int>();
+            //Console.WriteLine("Bubble Sort:");
+            //var bubble = new Algorithms.BubbleSort<int>();
             var rnd = new Random();
 
-            Console.WriteLine("Unsorted list:");
-            for (int i = 0; i < 10; i++)
-            {
-                bubble.Items.Add(rnd.Next(0, 100));
-                Console.Write(bubble.Items[i] + " ");
-            }
-            Console.WriteLine();
+            //Console.WriteLine("Unsorted list:");
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    bubble.Items.Add(rnd.Next(0, 100));
+            //    Console.Write(bubble.Items[i] + " ");
+            //}
+            //Console.WriteLine();
 
-            Console.WriteLine("Result of sorting:");
-            bubble.Sort();
-            for (int i = 0; i < 10; i++)
-            {
-                Console.Write(bubble.Items[i] + " ");
-            }
-            Console.WriteLine('\n');
+            //Console.WriteLine("Result of sorting:");
+            //bubble.Sort();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.Write(bubble.Items[i] + " ");
+            //}
+            //Console.WriteLine('\n');
             #endregion
 
             #region ShakerSort
-            Console.WriteLine("Shaker Sort:");
-            var shaker = new Algorithms.ShakerSort<int>();
+            //Console.WriteLine("Shaker Sort:");
+            //var shaker = new Algorithms.ShakerSort<int>();
 
-            Console.WriteLine("Unsorted list:");
-            for (int i = 0; i < 10; i++)
-            {
-                shaker.Items.Add(rnd.Next(0, 100));
-                Console.Write(shaker.Items[i] + " ");
-            }
-            Console.WriteLine();
+            //Console.WriteLine("Unsorted list:");
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    shaker.Items.Add(rnd.Next(0, 100));
+            //    Console.Write(shaker.Items[i] + " ");
+            //}
+            //Console.WriteLine();
 
-            Console.WriteLine("Result of sorting:");
-            shaker.Sort();
-            for (int i = 0; i < 10; i++)
-            {
-                Console.Write(shaker.Items[i] + " ");
-            }
-            Console.WriteLine('\n');
+            //Console.WriteLine("Result of sorting:");
+            //shaker.Sort();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.Write(shaker.Items[i] + " ");
+            //}
+            //Console.WriteLine('\n');
             #endregion
 
             #region InsertSort
-            Console.WriteLine("Insert Sort:");
-            var insert = new Algorithms.InsertSort<int>();
+            //Console.WriteLine("Insert Sort:");
+            //var insert = new Algorithms.InsertSort<int>();
+
+            //Console.WriteLine("Unsorted list:");
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    insert.Items.Add(rnd.Next(0, 100));
+            //    Console.Write(insert.Items[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //Console.WriteLine("Result of sorting:");
+            //insert.Sort();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.Write(insert.Items[i] + " ");
+            //}
+            //Console.WriteLine('\n');
+            #endregion
+
+            #region ShellSort
+            Console.WriteLine("Shell Sort:");
+            var shell = new Algorithms.ShellSort<int>();
 
             Console.WriteLine("Unsorted list:");
             for (int i = 0; i < 10; i++)
             {
-                insert.Items.Add(rnd.Next(0, 100));
-                Console.Write(insert.Items[i] + " ");
+                shell.Items.Add(rnd.Next(0, 100));
+                Console.Write(shell.Items[i] + " ");
             }
             Console.WriteLine();
 
             Console.WriteLine("Result of sorting:");
-            insert.Sort();
+            shell.Sort();
             for (int i = 0; i < 10; i++)
             {
-                Console.Write(insert.Items[i] + " ");
+                Console.Write(shell.Items[i] + " ");
             }
             Console.WriteLine('\n');
             #endregion
